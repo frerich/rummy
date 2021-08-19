15b2c6573e0d1c6157a3f6b12077b226480b7e50
Frerich Raabe <frerich.raabe@gmail.com>
2021-08-19 09:33:34 +0200

    Add Credo

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Rummy.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5.6", only: [:dev, :test], runtime: false},
     ]
   end
 
